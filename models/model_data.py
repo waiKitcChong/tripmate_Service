903d154891c8a4a1940dfe624733e6fd77043506
waiKitcChong <150654219+waiKitcChong@users.noreply.github.com>
2025-10-29 16:27:47 +0800

    Update main.py with latest changes

--- models/model_data.py
+++ models/model_data.py
@@ -5,7 +5,37 @@ def get_all_tables():
     Retrieve all data from Supabase tables.
     Returns a dictionary with table names as keys and their data as values.
     """
-    tables = ["users", "orders", "order_details", "payments"]
+    tables = [
+    "Booking",
+    "Business",
+    "Bussiness_Owner",
+    "Bussiness_Staff",
+    "Comments",
+    "Community",
+    "EmergencySupport",
+    "Hotel",
+    "HotelBookingDetails",
+    "Insurance",
+    "Itinerary",
+    "Location",
+    "Location_Rate",
+    "Menu",
+    "Promotion",
+    "Promotion_Target",
+    "RestBookingDetails",
+    "Rest_Schedule",
+    "Restaurant",
+    "Room",
+    "RoomType",
+    "Room_Schedule",
+    "SubComments",
+    "TableList",
+    "Tag",
+    "Tourists",
+    "TripPlan",
+    "User"
+]
+
     data = {}
 
     for table in tables:
