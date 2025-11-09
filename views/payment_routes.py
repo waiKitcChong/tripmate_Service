c972fb9e87ffe6a935186531fb43fa991af548eb
waiKitcChong <150654219+waiKitcChong@users.noreply.github.com>
2025-11-09 13:13:45 +0800

    Update main.py with latest changes

--- views/payment_routes.py
+++
@@ -1,9 +0,0 @@
-# views/payment_routes.py
-from flask import Blueprint
-from controllers.payment_controller import create_payment_intent
-
-payment_routes = Blueprint("payment_routes", __name__)
-
-@payment_routes.route("/create-payment-intent", methods=["POST"])
-def handle_create_payment_intent():
-    return create_payment_intent()
